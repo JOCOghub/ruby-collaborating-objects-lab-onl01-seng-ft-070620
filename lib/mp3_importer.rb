3783316e896da09bab79a5a1618630b210409202
Joseph cohen <jojocohen57@gmail.com>
2020-07-20 23:31:39 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,6 +1,10 @@
 class MP3Importer
    attr_accessor :path
   
+  def initialize(path)
+    @path = path
+  end 
+  
   def import(list_of_filenames)
     list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
   end
