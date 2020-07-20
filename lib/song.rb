9884a26e539064f20876591235aec90c462df65e
Joseph cohen <jojocohen57@gmail.com>
2020-07-20 23:36:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,8 @@ class Song
   end   
   
   def self.new_by_filename(filename)
-    song = self.new
+    
+    song = self.new(song)
     song.title = filename.split(" - ")[1]
     song.artist = Artist 
     song
