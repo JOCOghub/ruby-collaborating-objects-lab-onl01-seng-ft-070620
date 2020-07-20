13281d250185dbca16f62425f47db02df2c02814
Joseph cohen <jojocohen57@gmail.com>
2020-07-20 23:43:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,7 +20,7 @@ class Song
   end   
   
   def artist_name=(name) 
-    Artist.find_or_create_by_name(name)
+    self.artist.find_or_create_by_name(name)
     Artist.add_song(self)
   end   
     
